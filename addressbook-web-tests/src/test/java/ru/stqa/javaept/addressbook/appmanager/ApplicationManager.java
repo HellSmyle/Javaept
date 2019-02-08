7d047b28bc8024bcb41584ed3edd405239bf4e2b
HellSmyle <zozelenka@gmail.com>
2019-02-08 21:14:52 +0200

    ClassHelpersFinish

--- addressbook-web-tests/src/test/java/ru/stqa/javaept/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaept/addressbook/appmanager/ApplicationManager.java
@@ -1,8 +1,5 @@
 package ru.stqa.javaept.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -32,23 +29,7 @@ private SessionHelper sessionHelper;
     wd.quit();
   }
 
-  public boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
 
-  public boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 
   public GroupHelper getGroupHelper() {
     return groupHelper;
