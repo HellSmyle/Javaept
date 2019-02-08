c06e19d498d9fce90aa9c81ca961e0044076d3fb
HellSmyle <zozelenka@gmail.com>
2019-02-08 20:38:26 +0200

    ClassHelpersBeginning

--- addressbook-web-tests/src/test/java/ru/stqa/javaept/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaept/addressbook/tests/GroupCreationTests.java
@@ -9,13 +9,13 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
-    app.goToGroupPage();
-    app.initGroupCreation();
-    app.fillGroupForm(new GroupData("test1", "test2", "test3"));
-    app.submitGroupCreation();
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupCreation();
     // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=submit | ]]
-    app.returntoGroupPage();
-    app.wd.findElement(By.linkText("Logout")).click();
+    app.getGroupHelper().returntoGroupPage();
+    app.getGroupHelper().wd.findElement(By.linkText("Logout")).click();
   }
 
 
